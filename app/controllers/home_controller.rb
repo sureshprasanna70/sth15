f1266e1c739ea7c553ce5054585627df46d6dc1e
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-08 19:44:39 +0530

    Redirect and profile styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+       redirect_to "http://vision2015.in" 
+    else
+      redirect_to "/users/sign_in"
+    end
   end
 end
