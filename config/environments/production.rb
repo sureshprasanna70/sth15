25890cbd796ae467c52b54b96a2ad9dcd538f5f8
Ubuntu <ubuntu@ip-172-31-29-192.ap-southeast-1.compute.internal>
2015-02-09 19:30:19 +0000

    Prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => "login.vision2015.in"}
   config.action_mailer.smtp_settings = { :address => "mail.vision2015.in", 
                                          :port =>26,
-                                         :username=>"noreply@vision2015.in",
+                                         :user_name=>"noreply@vision2015.in",
                                          :password=>"n13a}qXT-Lg!",
                                          :enable_starttls_auto=>false,
                                          :authentication=>"plain"
