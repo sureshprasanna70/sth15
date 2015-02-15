5dd289cf0f6b07fefefe6ffcd607dbbb6f516e5f
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-15 21:59:33 +0530

    Validate presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :college ,presence:true
+  validates :department,presence:true
+  validates :name,presence:true
+  validates :year,presence:true
+
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
