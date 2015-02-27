142ab5091ba60235a5c1d5ed95e9847bf85e9d70
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-27 23:09:12 +0530

    Adding phone conf again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
-    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year)
+    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year,:phone)
   end
   def account_update_params 
-    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year)
+    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year,:phone)
   end
 end
