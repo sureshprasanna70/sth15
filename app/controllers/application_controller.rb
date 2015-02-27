11142db033495f23a2c34835abc9fc9aae474c9f
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-27 23:34:56 +0530

    Json only no csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, :if => :json_request?
   respond_to :html, :json
+  def json_request?
+        request.format.json?
+  end
 end
