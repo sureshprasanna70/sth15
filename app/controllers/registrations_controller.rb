c47695c0175d95677361904d7ad97e93e23eebdc
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-05-09 18:26:23 +0530

    Stripped off stylesheets

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
-    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year,:phone)
+    params.require(:user).permit(:email,:password,:password_confirmation,:name)
   end
   def account_update_params 
-    params.require(:user).permit(:email,:password,:password_confirmation,:name,:college,:department,:year,:phone)
+    params.require(:user).permit(:email,:password,:password_confirmation,:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :college ,presence:true
-  validates :department,presence:true
-  validates :name,presence:true
-  validates :year,presence:true
-  validates :phone,presence:true,numericality:true
-  devise :database_authenticatable, :registerable,:confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable
 end
