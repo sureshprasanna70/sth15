bb34c8c201247c415ac3d07bf1e00a6836c6b973
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-05-09 22:47:54 +0530

    sign in and sign up ui done

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,11 +2,11 @@ class ForumsController < ApplicationController
   before_action :set_forum,only: [:show, :edit, :update, :destroy]
   before_filter :sign_in_check
   respond_to :html
-  def selffeed
+  def index
     @forums = current_user.forums
     respond_with(@forums)
   end
-  def index
+  def selffeed
     @mapping=current_user.mapping.pluck(:forum_id)
     @forums = current_user.forums.where(:id=>@mapping)
     respond_with(@forums)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,11 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  Rails.application.config.to_prepare do
+  Devise::SessionsController.layout "sign_in"
+  Devise::RegistrationsController.layout proc { |controller| user_signed_in? ? "application" : "sign_in" }
+  Devise::ConfirmationsController.layout "sign_in"
+  Devise::UnlocksController.layout "sign_in"
+  Devise::PasswordsController.layout "sign_in"
+end
 end
